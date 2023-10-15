1bf285f92fc6755d7b37647eb881d2795c4597c9
keenet1 <keenet1@aol.com>
2023-10-15 16:45:10 -0500

    commit code updates

--- Leaflet-Part-1/js/logic.js
+++ Leaflet-Part-1/js/logic.js
@@ -57,7 +57,7 @@ function createAttributes(earthquakeData) {
 
 function createMap(earthquakes) {
     // Create tile layer
-    var grayscaleLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    var grayscale = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
         maxZoom: 18,
